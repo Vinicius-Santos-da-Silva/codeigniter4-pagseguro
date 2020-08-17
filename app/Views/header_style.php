393d10a624610e857d7ea56269252e9af3b6b22e
Guilherme Donada <guilhermedonada@gmail.com>
2020-08-17 03:49:12 -0300

    AJuste de tela de login

--- app/Views/header_style.php
+++ app/Views/header_style.php
@@ -4,4 +4,4 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="<?php echo site_url('assets/css/style.css');?>" rel="stylesheet">
+<link href="<?php echo site_url('public/assets/css/style.css');?>" rel="stylesheet">
--- app/Views/login.php
+++ app/Views/login.php
@@ -9,7 +9,7 @@
 <body>
 
     <?php echo view('navbar'); ?>
-
+<!-- 
     <div class="container">
         <form method="POST" action="<?php echo site_url('login/validar') ?>">
             
@@ -30,6 +30,130 @@
             <button type="submit" class="btn btn-primary">entrar</button>
         </form>
     </div>
+
+
+ -->
+
+
+
+
+
+
+
+
+
+<div class="d-flex align-items-center">
+    <div class="auto-container pb-5">
+   
+        <div class="row mt-4 ">
+            <div class="col-lg-6 col-md-12 col-sm-12 pr-5">
+
+            <?php if($this->session->markAsFlashdata('mensagem')){
+                echo $this->session->markAsFlashdata('mensagem');
+            } ?>
+                
+
+                <h1 class="fw-700">Data Analytics Techniques with 
+                    <span class="css-main-color">Detox</span> Systems.
+                </h1>
+                <p class="mt-4 fw-400 f-16 text-muted lh-30">Detox's real-time data management technologies, global data marketplaces, and award-winning customer service make our unstacked data solutions.</p>
+                <div class="mt-4"> 
+                    <ul class="list-item" style="list-style: none;">
+                        <li>
+                            <p class="pb-3 mb-0  d-flex text-muted f-18 fw-200">
+                                <span class="material-icons css-main-color pr-2 f-24">check_circle_outline</span>coisa boa asdasd asd
+                            </p>
+                        </li>
+                        <li>
+                            <p class="pb-3 mb-0  d-flex text-muted f-18 fw-200">
+                                <span class="material-icons css-main-color pr-2">check_circle_outline</span>coisdsdsd sa asa boa
+                            </p>
+                        </li>
+                        <li>
+                            <p class="pb-3 mb-0  d-flex text-muted f-18 fw-200">
+                                <span class="material-icons css-main-color pr-2">check_circle_outline</span>coidasd asdasd as dsa boa
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <button class="btn css-button">Ver Algo interessante</button>
+            
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div class="col-lg-6 col-md-12 col-sm-12 d-flex align-items-center">
+
+            <?php $this->session; ?>
+
+
+                <div role="form" class="css-div-form" >
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h4 class="fw-700 css-main-color mb-3">Entrar</h4>
+                        </div>
+                    </div>
+                    
+                   
+                  
+                    <form method="POST" action="<?php echo site_url('login/validar') ?>" class="css-form-cadastro">
+                        <div class="default-form" id="contact-form" >
+                            <div class="row ">
+                       
+                                <div class="col-lg-12 col-md-12 col-sm-12 form-group"> 
+                                    <span class="">
+                                        <input type="email" name="email" value="" size="40" class="form-control" aria-invalid="false" placeholder="Email">
+                                    </span>
+                                </div>
+
+                                <div class="col-lg-12 col-md-12  col-sm-12 form-group"> 
+                                    <span class="">
+                                        <input type="password" name="senha" value="" size="40" class="form-control" aria-invalid="false" placeholder="Senha">
+                                    </span>
+                                </div>
+
+                              
+                 
+                                <div class="col-lg-12 col-md-12 col-sm-12 form-group message-btn"> 
+                                   
+                                    <button type="submit" class="css-button-color">entrar</button>
+                                </div>
+                            </div>
+                        </div>  
+                    </form>
+                   
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
 
     
     <?php echo view('header_script'); ?>
