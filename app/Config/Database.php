c8ec8f02187cf23c0114280e748e471b64433410
root <root@ip-172-31-26-151.us-east-2.compute.internal>
2021-03-28 01:50:22 +0000

    Database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -31,9 +31,9 @@ class Database extends \CodeIgniter\Database\Config
 	 */
 	public $default = [
 		'DSN'      => '',
-		'hostname' => '3.21.242.90',
-		'username' => 'ec2-user',
-		'password' => 't44zg1g1',
+		'hostname' => 'localhost',
+		'username' => '',
+		'password' => '',
 		'database' => 'catavoto',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
