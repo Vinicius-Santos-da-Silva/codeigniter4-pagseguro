d57a0930914ddc665cce6e9222cc0961ac37f929
Vinicius Santos da Silva <vinicius.unisinos@hotmail.com>
2021-04-14 21:46:56 -0300

    controler para testar env

---
+++ app/Controllers/Test.php
@@ -0,0 +1,24 @@
+<?php namespace App\Controllers;
+
+use App\Models\UsuarioModel;
+use CodeIgniter\Controller;
+
+class Test extends Controller
+{
+
+    public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
+	{
+		parent::initController($request, $response, $logger);
+		$this->session = \Config\Services::session();	
+    }
+    
+	public function index()
+	{
+        echo PHP_EOL;
+        echo "teste env.";
+        echo PHP_EOL;
+
+        die(getenv('TESTE'));
+    }
+    
+}
